9d14d5ac3827396da7d32c2219f5fdc449613deb
praveensvk1408 <104765721+praveensvk1408@users.noreply.github.com>
2024-03-11 12:04:51 +0530

    file exists

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def read_json_file(file_path):
     with open(file_path, 'r') as json_file:
@@ -10,4 +11,9 @@ data = read_json_file('./data.json')
 
 for item in data['data']:
     print(item)
-    
+    # def fileExists = fileExists "${env.WORKSPACE}/../${item}/microservices/webbff/test/e2e/goReportJSONFile/getProduct_output.json"
+    file_path = f"../{item}/microservices/webbff/test/e2e/goReportJSONFile/getProduct_output.json"
+    if os.path.exists(file_path):
+        print(f"File '{file_path}' exists.")
+    else:
+        print(f"File '{file_path}' does not exist.")
